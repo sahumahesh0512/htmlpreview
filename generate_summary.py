82844b3b5ebf926bf76b906cbeea765965f5595f
sahumahesh0512 <91590622+sahumahesh0512@users.noreply.github.com>
2024-06-27 08:10:27 +0530

    Update generate_summary.py

--- generate_summary.py
+++ generate_summary.py
@@ -9,7 +9,9 @@ summary = data.get('summary', {})
 num_passed = summary.get('passed', 0)
 num_failed = summary.get('failed', 0)
 num_skipped = summary.get('skipped', 0)
-duration = summary.get('duration', 0)
+
+# Extract duration
+duration = data.get('duration', 0)  # Adjust this based on the correct location of duration in your JSON structure
 
 # Convert duration to minutes and seconds
 minutes, seconds = divmod(duration, 60)
@@ -19,4 +21,4 @@ with open('test_summary.md', 'w') as f:
     f.write('### Test Results Summary 🚀\n')
     f.write(f'| Test Result :test_tube: | Passed :green_circle: | Failed :x: | Skipped :heavy_minus_sign: | Time Duration :alarm_clock: |\n')
     f.write(f'| ---------------------- | ------- | ----------- | ----------- | ------------- |\n')
-    f.write(f'| Summary                | {num_passed}      | {num_failed}           | {num_skipped}           | {minutes}m {seconds}s       |\n')
+    f.write(f'| Summary                | {num_passed}      | {num_failed}           | {num_skipped}           | {int(minutes)}m {int(seconds)}s       |\n')
